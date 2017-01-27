14c5e4c2b25ecad246dd94d8a986d015cf381392
Nicolas Wormser <nicolas.wormser@gmail.com>
2017-01-27 17:43:18 +0400

    Add gqlClient module + graceful DI

---
+++ gqlClient.js
@@ -0,0 +1,33 @@
+module.exports = (endpoint, query, headers) => {
+  const url = require('url');
+  const locator = url.parse(endpoint);
+  let lib;
+
+  try {
+    // get the protocol from the URL of the endpoint
+    lib = require(locator.protocol.split(':')[0]);
+  } catch (err){
+    throw new Error(`Error in parsing the endpoints' protocol: ${err.message}`);
+  }
+
+  const options = {
+    hostname: locator.hostname,
+    path: locator.path,
+    method: 'POST',
+    headers: headers
+  };
+
+  // Construct the request object
+  const request = lib.request(options, res => {
+    res.setEncoding('utf8');
+    res.on('data', chunk => console.log(chunk));
+    res.on('end', () => console.log("request completed"));
+  });
+
+  request.on('error', err => new Error(`The request halted: ${err.message}`));
+
+  // Send the query
+  request.write(query);
+  request.end();
+}
+
